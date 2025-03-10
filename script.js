251a86009ded6d44d5b7e224711cabe70496ca5f
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2025-03-10 08:29:15 +0700

    add function promise

---
+++ script.js
@@ -0,0 +1,34 @@
+const token = ~~[Math.random() * 123456]
+
+const pictures = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"]
+
+// promise
+function login(username, callback) {
+    console.log('processing token user now...')
+    return new Promise((success, failed) => {
+        setTimeout(() => {
+            if (username !== "sabrina") failed('sorry wrong data')
+            success(token)
+        }, 200)
+    })
+}
+
+function getUser(token, callback) {
+    console.log('processing apiKey now...')
+    if (token)
+        setTimeout(() => {
+            callback({ apiKey: "xkey123" })
+        }, 500)
+}
+
+function getPictures(apiKey, callback) {
+    console.log('processing pictures now....')
+    if (apiKey)
+        setTimeout(() => {
+            callback({ pic: pictures })
+        }, 1500)
+
+}
+
+const user = login("sabrina")
+console.log(user)
