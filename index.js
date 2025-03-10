56f10e7706b9359e34dfa5f68e9925e55bedda12
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2025-03-10 08:14:49 +0700

    perubahan pada getuser dll callback

--- index.js
+++ index.js
@@ -21,30 +21,41 @@ const token = ~~[Math.random() * 123456]
 const pictures = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"]
 function login(username, callback) {
     setTimeout(() => {
-        callback ({token, username})
-}, 200)
-    
+        callback({ token, username })
+    }, 200)
+
 }
 
 function getUser(token, callback) {
-    if(token)
-        setTimeout(() =>{
-     callback ({apiKey: "xkey123"})
-    }, 500)
+    if (token)
+        setTimeout(() => {
+            callback({ apiKey: "xkey123" })
+        }, 500)
 }
 
-function getPictures(apiKey) {
-    if(apiKey) return  pictures
+function getPictures(apiKey, callback) {
+    console.log('processing pictures now....')
+    if (apiKey)
+        setTimeout(() => {
+            callback({pic: pictures}) 
+        }, 1500)
+
 }
 
-login("sabrina", function(response){
-    const {token} = response
-    const apiKey = getUser(token)
-    console.log(apiKey);
+login("sabrina", function (response) {
+    const { token } = response
+    getUser(token, function (response) {
+        const { apiKey } = response
+        getPictures(apiKey, function (response) {
+            const {pic} = response
+            console.log(pic)
+    })
+})
 });
-console.log(user.token)
-// const apiKey = getUser(user.token);
-console.log(apiKey);
 
-const getUserPicture = getPictures(apiKey)
-console.log(getUserPicture)
+// console.log(user.token)
+// // const apiKey = getUser(user.token);
+// console.log(apiKey);
+
+// const getUserPicture = getPictures(apiKey)
+// console.log(getUserPicture)
