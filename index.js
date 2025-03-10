fb5e33ba1b73c6b6c301e2b4c9d2db1d98d771ee
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2025-03-10 07:14:10 +0700

    add function token login username

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ function satu() {
     console.log("satu");
 }
 function dua() {
-    console.log("dua");
+    console.log('function 2 mau di eksekusi...')
+    setTimeout(() => {
+        console.log("dua");
+    }, 3000)
+
 }
 function tiga() {
     console.log("tiga");
@@ -11,3 +15,16 @@ function tiga() {
 satu();
 dua();
 tiga();
+
+const token = ~~[Math.random() * 123456]
+function login(username) {
+    return {token, username}
+}
+
+function getUser() {
+//
+}
+
+const user = login("sabrina");
+
+console.log(user);
